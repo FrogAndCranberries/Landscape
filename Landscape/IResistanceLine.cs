7c26de09560ca269c67f0db3f357270f7386b0d4
JeffSeirre <strakmi@gmail.com>
2021-04-06 17:35:41 +0200

    Add IResistanceLine interface implemented by all baselines

---
+++ Landscape/IResistanceLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using cAlgo.API;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
+using cAlgo.Indicators;
+
+namespace cAlgo
+{
+    interface IResistanceLine
+    {
+        void Visualize(Chart chart);
+    }
+}
